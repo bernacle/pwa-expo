1d35012c74889ffa55f69b3952b2b20ad40a2c1f
F1694326 Bruno Raphael Bifaroni Simplicio <brunoraphael@bb.com.br>
2020-06-25 23:17:50 -0300

    Add Routes and Expo Cameraˆ

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, Image, Text, StyleSheet } from 'react-native';
 
 // import { Container } from './styles';
 
-const Main: React.FC = () => {
-    return <View />;
+interface Member {
+    login: string;
+    avatar_url: string;
 }
 
-export default pages;
+
+const Main: React.FC = () => {
+    const [members, setMembers] = useState<Member[]>([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/facebook/members')
+            .then(response => {
+                response.json().then(data => {
+                    setMembers(data)
+                })
+            })
+    }, [])
+
+    return (
+        <FlatList
+            contentContainerStyle={{ padding: 24 }}
+            data={members}
+            keyExtractor={member => member.login}
+            renderItem={({ item: member }) => (
+                <View style={styles.member}>
+                    <Image style={styles.image} width={30} height={30} source={{ uri: member.avatar_url }} />
+                    <Text>{member.login}</Text>
+                </View>
+            )}
+        />
+    )
+}
+
+
+const styles = StyleSheet.create({
+    member: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    image: {
+        width: 48,
+        height: 48,
+        borderRadius: 24,
+        marginRight: 16
+    }
+})
+
+
+export default Main;
